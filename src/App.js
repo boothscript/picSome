b1c15e050371040402bd44c95f79e20f194778e6
Steve Booth <scriptmcuk@gmail.com>
2019-12-02 14:05:45 +0000

    Created photo and cart pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 
-
+import Photos from './pages/Photos'
+import Cart from './pages/Cart'
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Switch>
+        <Route exact path="/">
+          <Photos />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+      </Switch>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/pages/Photos.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Photos() {
+
+    return (
+        <h1>Photos Page</h1>
+    )
+}
+
+export default Photos
